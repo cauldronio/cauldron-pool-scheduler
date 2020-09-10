dcc6045c603fdd97e48db059d6af1fa47d4dc1d3
JJMerchante <jj.merchante@gmail.com>
2020-09-10 10:17:25 +0200

    Update Dockerfile and Django version
    This commit updates the entrypoint to include a migration to the database
    that is being used. It also updates the Django version to the latest one (3.1.1)
    
    Signed-off-by: JJMerchante <jj.merchante@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-python3 manage.py schedworker
+function log_message {
+  echo "["`date -u "+%Y-%m-%d %H:%M:%S UTC"`"]" $*
+}
+
+log_message "Migrate"
+until python manage.py migrate; do
+    log_message "Error in Migrate.. Retry"
+    sleep 5
+done
+log_message "Done"
+
+log_message "Start the worker"
+python manage.py schedworker
